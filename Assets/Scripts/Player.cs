6770c696f26be47bd5f4413cc2254718184461c7
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-07-31 06:02:31 +0900

    기본적인 이동 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform attackBoxPos;
+    public Vector2 boxSize;
     private Rigidbody2D rb;
+    public float damage;
     public float moveSpeed;
     public float jumpPower;
+    public float coolTime;
     private int jumpCount;
+    private float curTime;
     void Start()
     {
         jumpCount = 0;
+        curTime = 0;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Move();
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && curTime <= 0)
+            Attack();
+        curTime -= Time.deltaTime;
     }
 
     private void Move()
@@ -30,15 +40,42 @@ public class Player : MonoBehaviour
         {
             jumpCount++;
             rb.velocity = Vector3.zero;
-            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Attack()
+    {
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(attackBoxPos.position, boxSize, 0);
+        for (int i = 0; i < collider2Ds.Length; i++)
+        {
+            if (collider2Ds[i].CompareTag("Enemy"))
+            {
+                TakeDamaged(collider2Ds[i]);
+            }
+        }
+        curTime = coolTime;
+    }
+
+    private void TakeDamaged(Collider2D collider2D)
+    {
+        collider2D.GetComponent<Enemy>().hp -= damage;
+
+        float direction = transform.localScale.x > 0 ? 2 : -2;
+        collider2D.GetComponent<Rigidbody2D>().AddForce(new Vector2(direction, 0), ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Floor"))
         {
             jumpCount = 0;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(attackBoxPos.position, boxSize);
     }
 }
