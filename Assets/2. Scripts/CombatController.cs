bfb1f0358ba0b973bfe332bdbd3001f929a2d72e
kyw04 <85551017+kyw04@users.noreply.github.com>
2026-05-05 21:40:55 +0900

    feat: Combat Controller

---
+++ Assets/2. Scripts/CombatController.cs
@@ -0,0 +1,14 @@
+﻿public class CombatController : IAttacker, IDamageable
+{
+    
+    
+    public void Attack(IDamageable target)
+    {
+        target.TakeDamage(1);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        
+    }
+}
