fc68b9469292a6f0717795e51f57c9666d151d95
kyw04 <85551017+kyw04@users.noreply.github.com>
2026-06-27 23:16:03 +0900

    feat: dash count UI

---
+++ Assets/2. Scripts/UI/DashCountUI.cs
@@ -0,0 +1,22 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class DashCountUI : MonoBehaviour
+{
+    private Player player;
+    [SerializeField] private TextMeshProUGUI maxCountText;
+    [SerializeField] private TextMeshProUGUI countText;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        int count = player.MaxDashCount - player.DashCount;
+        maxCountText.text = player.MaxDashCount.ToString();
+        countText.text = count.ToString();
+    }
+}
