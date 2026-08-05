4569fc1fe3d03fbb24c6b0f52104093c82b6bbd3
kyw04 <85551017+kyw04@users.noreply.github.com>
2026-08-05 14:30:22 +0900

    refactor: ItemBox 도입으로 불필요해진 StageData의 랜덤 보상 풀 제거

--- Assets/2. Scripts/Core/Stage/StageData.cs
+++ Assets/2. Scripts/Core/Stage/StageData.cs
@@ -6,14 +6,12 @@ public class StageData : ScriptableObject
 {
     public StageNode startNode;
     public List<StageNode> battleNodes = new();
-    public List<SkillData> skillRewardPool = new();
-    public List<Weapon> weaponRewardPool = new();
 
     public StageNode shopNode;
     public int shopRoomDistance;
-    
+
     public StageNode bossNode;
     public int bossRoomDistance;
-    
+
     public StageData nextStage;
 }
