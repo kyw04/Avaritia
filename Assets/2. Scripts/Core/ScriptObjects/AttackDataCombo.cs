22bbe6f1946ea8ea32408e31a80c64aa907eed9a
kyw04 <85551017+kyw04@users.noreply.github.com>
2026-08-14 20:16:42 +0900

    refactor: AttackDataCombo.duration를 cooldown으로 이름 변경 (SkillData.cooldown 컨벤션에 맞춤)

--- Assets/2. Scripts/Core/ScriptObjects/AttackDataCombo.cs
+++ Assets/2. Scripts/Core/ScriptObjects/AttackDataCombo.cs
@@ -6,5 +6,5 @@ public class AttackDataCombo : ScriptableObject
 {
     public int Count => datas.Count;
     public List<AttackData> datas = new();
-    public float duration;
+    public float cooldown;
 }
