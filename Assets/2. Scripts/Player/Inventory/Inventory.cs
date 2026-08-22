c07c71fe4b40ae00d5c5d73e8dd0f8ed92b46fbf
kyw04 <85551017+kyw04@users.noreply.github.com>
2026-08-23 03:40:20 +0900

    feat: Inventory에 선택된 아이템 상태 추가

--- Assets/2. Scripts/Player/Inventory/Inventory.cs
+++ Assets/2. Scripts/Player/Inventory/Inventory.cs
@@ -5,6 +5,9 @@ public class Inventory
     private readonly List<IInventoryItem> items = new();
 
     public IReadOnlyList<IInventoryItem> Items => items;
+    public IInventoryItem SelectedItem { get; private set; }
+
     public void Add(IInventoryItem item) => items.Add(item);
     public bool Remove(IInventoryItem item) => items.Remove(item);
+    public void Select(IInventoryItem item) => SelectedItem = item;
 }
